11602cffe06ad99395462ffb6e13c3e2fa0d2990
yimmit303 <timsteiner303@gmail.com>
2020-02-01 14:24:51 -0500

    Makes camera shake

--- Scripts/CameraShake.gd
+++ Scripts/CameraShake.gd
@@ -1,8 +1,35 @@
 extends Node
 
 static func shake_camera(camera : Node2D, intensity):
-	var effect_scale = 1
-	var intial_vector = Vector2(randf() * 2 - 1, randf() * 2 - 1)
-	print(intial_vector)
-	camera.position
+	var initial_position = camera.offset
 	
+	var decay = 0.8
+	
+	var effect_scale = 1
+	var shake_dist = 40 * intensity
+	var move_vector = Vector2(0, 1)#Vector2(randf() * 2 - 1, randf() * 2 - 1)
+	var direction = -1 * move_vector
+	
+	var tween = Tween.new()
+	camera.add_child(tween)
+	
+	tween.interpolate_property(camera, "offset", 
+	camera.offset, camera.offset + (move_vector * shake_dist * effect_scale)
+	, 0.1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+	tween.start()
+	yield(tween, "tween_completed")
+	
+	var last_moved = shake_dist
+	
+	for i in range(10):
+		tween.interpolate_property(camera, "offset", 
+		camera.offset, camera.offset + (direction * shake_dist * effect_scale) + (direction * last_moved)
+		, 0.1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+		tween.start()
+		last_moved = shake_dist * effect_scale
+		direction *= -1
+		effect_scale *= decay
+		yield(tween, "tween_completed")
+	
+	camera.offset = initial_position
+	tween.queue_free()
