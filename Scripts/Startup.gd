eea9b81068388f05a8346375107899b16c58e620
yimmit303 <timsteiner303@gmail.com>
2020-01-31 21:53:47 -0500

    Changed the scrap dict to scrap list

--- Scripts/Startup.gd
+++ Scripts/Startup.gd
@@ -6,8 +6,7 @@ func _ready():
 func generate_scrap_dictionary(path):
 	var dir = Directory.new()
 	
-	var scrap_dict = {}
-	var count = 0
+	var scrap_list = []
 	
 	if dir.open(path) == OK:
 		dir.list_dir_begin()
@@ -15,11 +14,10 @@ func generate_scrap_dictionary(path):
 		while (file_name != ""):
 			if dir.current_is_dir():
 				if file_name != "." and file_name != "..":
-					scrap_dict[count] = file_name
-					count += 1
+					scrap_list.append(file_name)
 					print("Found directory: " + file_name)
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
-	print(scrap_dict)
-	Globals.SCRAP_DICTIONARY = scrap_dict
+	print(scrap_list)
+	Globals.SCRAP_DICTIONARY = scrap_list
