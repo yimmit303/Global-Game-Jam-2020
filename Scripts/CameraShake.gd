6a207225397e9735814eb2bf83744b639b5eb6ad
yimmit303 <timsteiner303@gmail.com>
2020-02-01 14:49:35 -0500

    Update CameraShake.gd

--- Scripts/CameraShake.gd
+++ Scripts/CameraShake.gd
@@ -1,13 +1,15 @@
 extends Node
 
 static func shake_camera(camera : Node2D, intensity):
-	var initial_position = camera.offset
+	
+	camera.offset = Vector2(0,0)
 	
 	var decay = 0.8
 	
 	var effect_scale = 1
-	var shake_dist = 40 * intensity
-	var move_vector = Vector2(0, 1)#Vector2(randf() * 2 - 1, randf() * 2 - 1)
+	var shake_dist = 10 * intensity
+	randomize()
+	var move_vector = Vector2(randf() * 2 - 1, randf() * 2 - 1)
 	var direction = -1 * move_vector
 	
 	var tween = Tween.new()
@@ -24,12 +26,12 @@ static func shake_camera(camera : Node2D, intensity):
 	for i in range(10):
 		tween.interpolate_property(camera, "offset", 
 		camera.offset, camera.offset + (direction * shake_dist * effect_scale) + (direction * last_moved)
-		, 0.1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+		, 0.05, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
 		tween.start()
 		last_moved = shake_dist * effect_scale
 		direction *= -1
 		effect_scale *= decay
 		yield(tween, "tween_completed")
 	
-	camera.offset = initial_position
+	camera.offset = Vector2(0,0)
 	tween.queue_free()
