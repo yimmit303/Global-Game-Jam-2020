adccddf763359674861be004804740585db8ca3b
cheapscate98 <35314966+cheapscate98@users.noreply.github.com>
2020-02-01 00:54:27 -0500

    Enemy AI progress
    Finished the ability for the AI to acquire a target and added in the beginnings of the ability of the AI to approach the target.

--- Scripts/Enemy_Basic.gd
+++ Scripts/Enemy_Basic.gd
@@ -1,42 +1,57 @@
 extends Node2D
 
+#Values intended for logic
 export(NodePath) var goal;
+var target: Node2D
+var target_position;
 var state = "acquiring";
-var states = ["acquiring", "persuing", "attacking"];
+var states = ["acquiring", "persuing", "attacking"]; #Not actually used, just here for convenience
+
+#Fixed Values for movement
 var outer_diameter = 500;
 var inner_diameter = 300;
-var attack_speed: float = 20;
+var attack_speed: float = 400;
+var rotate_speed: float = 2
+var acceleration: float = 200;
+
+#Values for constant manipulation
 var cooldown: float = 0;
+var velocity = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	target = get_node(goal)
 	pass # Replace with function body.
 
-func acquire(var delta) -> bool:
+
+
+func acquire(var delta):
 	#Get direction of player
-	var player_vec: Vector2 = self.global_position - goal.global_position;
-	player_vec = player_vec.normalized()
-	var enemy_heading = Vector2(cos(self.rotation_degrees), sin(self.rotation_degrees));
-	var dir = player_vec.dot(enemy_heading)
-	if dir < 0:
-		self.rotate(delta);
-	if dir > 0:
-		self.rotate(delta);
-	
-	player_vec= self.global_position - goal.global_position;
-	player_vec = player_vec.normalized()
-	enemy_heading = Vector2(cos(self.rotation_degrees), sin(self.rotation_degrees));
-	dir = player_vec.dot(enemy_heading)
-	if dir * dir < .1:
-		return true;
-	else:
-		return false;
-	
-func pursue() -> bool:
+	var angle = self.get_angle_to(target.global_position);
+	if angle < 0:
+		self.rotate(-delta * rotate_speed);
+	elif angle > 0:
+		self.rotate(delta * rotate_speed);
+		
+	angle = self.get_angle_to(target.global_position);
+	if abs(angle) < .05:
+		self.state = "pursuing";
+
+
+
+func pursue(var delta):
+	var distance_to_target = (target.global_position - self.global_position).length() - 300;
+	if self.velocity < attack_speed:
+		self.velocity += delta * acceleration;
+	self.position += self.global_transform.x * self.velocity * delta;
+	return true;
+
+
+
+func attack(var delta):
 	return true;
 	
-func attack() -> bool:
-	return true;
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -44,13 +59,10 @@ func _process(delta):
 	cooldown -= 1 * delta;
 	
 	if state == "acquiring":
-		if acquire(delta) == true:
-			state = "pursuing";
+		acquire(delta);
 		
-	elif state == "persuing":
-		if pursue() == true:
-			state = "attacking";
+	elif state == "pursuing":
+		pursue(delta);
 		
 	elif state == "attacking":
-		if attack() == true:
-			state = "acquiring";
+		attack(delta);
