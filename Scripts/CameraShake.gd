19a1aad1283b0966761ba37b6d321881c58798dd
yimmit303 <timsteiner303@gmail.com>
2020-02-01 13:13:52 -0500

    Create CameraShake.gd

---
+++ Scripts/CameraShake.gd
@@ -0,0 +1,8 @@
+extends Node
+
+static func shake_camera(camera : Node2D, intensity):
+	var effect_scale = 1
+	var intial_vector = Vector2(randf() * 2 - 1, randf() * 2 - 1)
+	print(intial_vector)
+	camera.position
+	
