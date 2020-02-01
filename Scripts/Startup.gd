c33c24719c12f2932de1d80f289c5ab162c485a1
yimmit303 <timsteiner303@gmail.com>
2020-02-01 01:10:33 -0500

    Removed print statements

--- Scripts/Startup.gd
+++ Scripts/Startup.gd
@@ -15,9 +15,7 @@ func generate_scrap_dictionary(path):
 			if dir.current_is_dir():
 				if file_name != "." and file_name != "..":
 					scrap_list.append(file_name)
-					print("Found directory: " + file_name)
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
-	print(scrap_list)
 	Globals.SCRAP_DIR_LIST = scrap_list
